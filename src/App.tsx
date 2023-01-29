171a874674fca9ceb7134ce9b3f92bc26ff9951a
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-01-29 16:53:24 +0530

    feat: added online status check

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import { Footer, Header } from './components';
+import { useOnline } from './hooks/useOnline';
 
 const AppLayout = () => {
+  const isOnline = useOnline();
+
+  if (!isOnline) {
+    return (
+      <Fragment>
+        <h1>Connection Error!</h1>
+        <h2>Please check your internet connection and try again.</h2>
+      </Fragment>
+    );
+  }
+
   return (
     <Fragment>
       <Header />
