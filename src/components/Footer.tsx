813b4ac13e15b366cf96cd8e1a9b110baeca7d9b
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-02-02 02:02:48 +0530

    fix: updated footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,64 @@
+import instagram from '../assets/instagram.webp';
+import twitter from '../assets/twitter.webp';
+import facebook from '../assets/facebook.webp';
+import pintrest from '../assets/pintrest.webp';
+
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-black p-4">
-      <div className="flex gap-4 justify-between border-b-1 border-white py-4">
-        <div className="text-gray">
-          <div>COMPANY</div>
-          <ul className="list-none text-white">
-            <li>About us</li>
-            <li>Careers</li>
-            <li>Swiggy Instamart</li>
-          </ul>
+    <footer className='bg-black pt-12 px-12'>
+      <div className='md:mx-32 sm:mx-0 flex flex-col'>
+        <div className='flex flex-wrap gap-4 justify-between border-b-[1px] border-white py-4'>
+          <div>
+            <div className='text-gray'>COMPANY</div>
+            <ul className='list-none text-white mt-4'>
+              <li>About us</li>
+              <li>Careers</li>
+              <li>Swiggy Instamart</li>
+            </ul>
+          </div>
+          <div>
+            <div className='text-gray'>CONTACT</div>
+            <ul className='list-none text-white mt-4'>
+              <li>Help &amp; Support</li>
+              <li>Partner with us</li>
+              <li>Ride with us</li>
+            </ul>
+          </div>
+          <div>
+            <div className='text-gray'>LEGAL</div>
+            <ul className='list-none text-white mt-4'>
+              <li>Terms &amp; Conditions</li>
+              <li>Refund &amp; Cancellation</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
         </div>
-        <div className="text-gray">
-          <div>CONTACT</div>
-          <ul className="list-none  text-white">
-            <li>Help &amp; Support</li>
-            <li>Partner with us</li>
-            <li>Ride with us</li>
-          </ul>
+        <div className='flex gap-4 flex-col md:flex-row flex-nowrap justify-between text-white py-4'>
+          <div className='flex gap-4 justify-between'>
+            <div>Swiggy</div>
+            <div>&copy; {year} Swiggy</div>
+          </div>
+          <div className='flex gap-6'>
+            <img
+              className='w-6 hover:scale-110'
+              src={facebook}
+              alt='facebook'
+            />
+            <img
+              className='w-6 hover:scale-110'
+              src={pintrest}
+              alt='pinterest'
+            />
+            <img
+              className='w-6 hover:scale-110'
+              src={instagram}
+              alt='instagram'
+            />
+            <img className='w-6 hover:scale-110' src={twitter} alt='twitter' />
+          </div>
         </div>
-        <div className="text-gray">
-          <div>LEGAL</div>
-          <ul className="list-none  text-white">
-            <li>Terms &amp; Conditions</li>
-            <li>Refund &amp; Cancellation</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </div>
-      </div>
-      <div className="flex justify-between text-white py-4">
-        <div>Swiggy</div>
-        <div>&copy; {year} Swiggy</div>
-        <div>Icons</div>
       </div>
     </footer>
   );
