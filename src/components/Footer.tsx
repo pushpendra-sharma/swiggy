895fc0a6fa1a979c80671e221b1509aabd647454
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-02-08 00:32:19 +0530

    feat(style): added footer nav for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,58 @@ import twitter from '../assets/twitter.webp';
 import facebook from '../assets/facebook.webp';
 import pintrest from '../assets/pintrest.webp';
 import footer from '../assets/footer.webp';
+import icon from '../assets/icons/swiggy-grey.svg';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className='bg-black pt-12 px-4 md:px-8 lg:px-12'>
-      <div className='mx-0 md:mx-16 lg:mx-32 flex flex-col'>
-        <div className='flex flex-wrap gap-12 justify-between border-b-[1px] border-white py-4'>
+    <footer className='bg-white pt-2 px-4 mt-auto fixed bottom-0 w-full z-10 shadow-inner sm:shadow-none sm:bg-black sm:pt-4 lg:pt-12 md:px-8 lg:px-12'>
+      <ul className='flex justify-between items-center text-[10px] font-medium text-gray-medium opacity-75 sm:hidden'>
+        <li className=''>
+          <Link to='/'>
+            <div className='flex flex-col items-center gap-[1px]'>
+              <img src={icon} alt='swiggy' className='w-6' />
+              <span>Swiggy</span>
+            </div>
+          </Link>
+        </li>
+        <li className=''>
+          <Link to='search'>
+            <div className='flex flex-col items-center gap-[1px]'>
+              <span className='material-symbols-outlined'>search</span>
+              <span>Search</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to='instamart'>
+            <div className='flex flex-col items-center gap-[1px]'>
+              <span className='material-symbols-outlined'>storefront</span>
+              <span>Instamart</span>
+            </div>
+          </Link>
+        </li>
+        <li className=''>
+          <Link to='checkout'>
+            <div className='flex flex-col items-center gap-[1px]'>
+              <span className='material-symbols-outlined'>shopping_bag</span>
+              <span>Cart</span>
+            </div>
+          </Link>
+        </li>
+        <li className=''>
+          <Link to='account'>
+            <div className='flex flex-col items-center gap-[1px]'>
+              <span className='material-symbols-outlined'>person</span>
+              <span>Account</span>
+            </div>
+          </Link>
+        </li>
+      </ul>
+      <div className='mx-0 hidden flex-col sm:flex sm:mx-4 md:mx-8 lg:mx-16'>
+        <div className='hidden flex-wrap gap-12 justify-between border-b-[1px] border-white py-4 sm:flex'>
           <div>
             <div className='text-gray'>COMPANY</div>
             <ul className='list-none text-white mt-4'>
@@ -36,7 +80,7 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div className='flex flex-col gap-4 lg:flex-row flex-nowrap justify-between text-white py-4 lg:items-center'>
+        <div className='hidden gap-4 flex-nowrap justify-between text-white py-4 items-center sm:flex lg:flex-row'>
           <div>
             <img className='w-36' src={footer} alt='swiggy logo' />
           </div>
