cb8443a6756e1a1a6b78b6eda37b426ddbad053f
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-02-08 00:31:42 +0530

    feat(style): updated header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,15 @@
 import { Link } from 'react-router-dom';
+import Address from './Address';
 import Title from './Title';
 
 const Header = () => {
   return (
-    <header className='px-4 py-2 md:px-8 lg:px-12'>
-      <div className='mx-0 md:mx-16 lg:mx-32 flex items-center justify-between'>
+    <header className='px-4 py-2 md:px-8 lg:px-12 fixed top-0 w-full z-10 bg-white shadow-sm'>
+      <div className='mx-0 sm:mx-4 md:mx-8 lg:mx-16 flex gap-4 items-center justify-between'>
         <Title />
-        <div className='flex mr-8'>
-          <Link to='address'>
-            <div className='flex items-center gap-2 text-sm'>
-              <span className='hover:text-orange font-bold border-b-2'>
-                Home
-              </span>
-              <span className='text-gray-light hover:opacity-75'>
-                Sector-1, Noida, U.P.
-              </span>
-              <span className='material-symbols-outlined text-orange'>
-                expand_more
-              </span>
-            </div>
-          </Link>
-        </div>
-        <ul className='flex justify-between items-center gap-6 font-medium text-gray-medium'>
-          <li className=''>
+        <Address />
+        <ul className='flex justify-between shrink items-center gap-4 font-medium text-gray-medium'>
+          <li className='hidden sm:inline'>
             <Link to='search'>
               <div className='flex items-center gap-2 hover:text-orange'>
                 <span className='material-symbols-outlined'>search</span>
@@ -30,7 +17,7 @@ const Header = () => {
               </div>
             </Link>
           </li>
-          <li>
+          <li className='hidden sm:inline'>
             <Link to='offers'>
               <div className='flex items-center gap-2 hover:text-orange'>
                 <span className='material-symbols-outlined'>percent</span>
@@ -38,7 +25,7 @@ const Header = () => {
               </div>
             </Link>
           </li>
-          <li>
+          <li className='hidden sm:inline'>
             <Link to='support'>
               <div className='flex items-center gap-2 hover:text-orange'>
                 <span className='material-symbols-outlined'>
@@ -48,15 +35,16 @@ const Header = () => {
               </div>
             </Link>
           </li>
-          <li>
+          <li className=''>
             <Link to='account'>
               <div className='flex items-center gap-2 hover:text-orange'>
-                <span className='material-symbols-outlined'>person</span>
-                <span>Sign in</span>
+                {/* <span className='material-symbols-outlined'>person</span> */}
+                <span className='material-symbols-outlined'>account_circle</span>
+                <span className='hidden sm:inline'>Sign in</span>
               </div>
             </Link>
           </li>
-          <li>
+          <li className='hidden sm:inline'>
             <Link to='checkout'>
               <div className='flex items-center gap-2 hover:text-orange'>
                 <span className='material-symbols-outlined'>shopping_bag</span>
