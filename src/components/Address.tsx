139f1a3e156a753fabf51cf008b3dca61905757a
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-02-08 00:30:37 +0530

    feat(style): updated address styling

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,10 +1,21 @@
-import { Search } from '.';
-
 const Address = () => {
   return (
-    <>
-      <Search />
-    </>
+    <div className='flex flex-col sm:flex-row sm:items-center sm:gap-2 sm:mr-auto'>
+      <div className='flex gap-2'>
+        <span className='material-symbols-outlined font-extrabold text-orange sm:hidden'>
+          pin_drop
+        </span>
+        <span className='font-bold sm:text-gray-medium sm:text-sm hover:text-orange sm:border-b-2'>
+          Home
+        </span>
+      </div>
+      <span className='text-xs text-gray-light hover:opacity-75 truncate'>
+        Sector-1, Noida, U.P.
+      </span>
+      <span className='material-symbols-outlined hidden text-orange sm:inline'>
+        expand_more
+      </span>
+    </div>
   );
 };
 
