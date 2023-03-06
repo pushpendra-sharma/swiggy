7c8dd7fe285b71b623fcc4e67a194fe61697cf60
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-06 22:43:30 +0530

    refactor: removed unnecessary code

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react';
-import { createPortal } from 'react-dom';
 import { Link } from 'react-router-dom';
 import { Carousel, RestaurantCard, ShimmerRestaurant } from '.';
 import { useGetAllRestaurantsQuery } from '../redux/api';
-import Modal from './Modal';
 
 const Body = () => {
-  const [show, setShow] = useState(false);
   const { data, isLoading, error, isSuccess } = useGetAllRestaurantsQuery();
 
   return (
-    <div className='pt-20'>
+    <div className='my-20'>
       <Carousel />
       <div className='mx-0 px-4 py-2 sm:mx-4 md:px-8 md:mx-8 lg:px-12 lg:mx-16'>
         <div className='flex flex-col gap-8 md:flex-row md:flex-wrap'>
@@ -32,11 +28,6 @@ const Body = () => {
                 ))}
             </>
           )}
-          {show &&
-            createPortal(
-              <Modal onClose={() => setShow(false)} title='' description='' />,
-              document.body
-            )}
         </div>
       </div>
     </div>
