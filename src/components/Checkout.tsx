a32bfd876f2dad94d7508f7689912e51e5d93801
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-09 22:17:15 +0530

    fix: updated cart items length

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -3,11 +3,11 @@ import { RootState } from '../redux/store';
 
 const Checkout = () => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
-
+  const length = Object.keys(cartItems).length;
   return (
     <div>
       <h3>Checkout page</h3>
-      <p>Items in cart: {cartItems.length}</p>
+      <p>Items in cart: {length}</p>
     </div>
   );
 };
