71a9aab3085f3bdc32aa80c3a865f97fddb7de40
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-09 08:19:45 +0530

    feat: added search suggestions on ui

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,61 @@
+import { useEffect, useState } from 'react';
+import { fetchQueryResult } from '../redux/features/search/searchSlice';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+
+type SEARCH_PROPS = {
+  hint: string;
+  callback: () => void;
+};
+
 const Search = () => {
+  const [query, setQuery] = useState('');
+  const [show, setShow] = useState(false);
+
+  const data = useAppSelector(state => state.search.data);
+  const dispatch = useAppDispatch();
+
+  const suggestions = data.find(item => item.query === query)?.suggestions;
+
+  const fetchSuggestions = (text: string) => {
+    dispatch(fetchQueryResult(text));
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => fetchSuggestions(query), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [query]);
+
   return (
-    <>
-      <input type="search" placeholder="Search bar" />
-    </>
+    <div className='mx-0 px-4 py-2 sm:mx-4 md:px-8 md:mx-8 lg:px-12 lg:mx-16 my-20'>
+      <div className='m-2 p-2 sm:mx-4 sm:px-4 md:mx-8 md:px-8 lg:mx-16 lg:px-16'>
+        <div className='h-12 px-2 flex justify-between items-center border border-gray-light rounded-md '>
+          <input
+            className='p-2 w-full text-ellipsis font-extralight text-sm focus:outline-none '
+            type='search'
+            placeholder='Search for restaurants and food'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            autoFocus
+            onFocus={() => setShow(true)}
+            onBlur={() => setShow(false)}
+          ></input>
+          {!show && (
+            <span className='material-symbols-outlined font-extralight'>
+              search
+            </span>
+          )}
+        </div>
+        {show && (
+          <ul className='relative py-2 px-4 flex flex-col gap-1 max-h-96 overflow-y-scroll border border-gray-light border-t-0'>
+            {suggestions &&
+              suggestions.map(item => <li className='hover:bg-[#e5e7eb] cursor-pointer' key={item.text}>{item.text}</li>)}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 };
 
