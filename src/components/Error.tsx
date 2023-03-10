00f426422f5ab095e81b20b1530d5fd0ca4cd2a3
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-10 22:23:27 +0530

    fix: updated error route layout

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,16 +1,21 @@
 import { useRouteError } from 'react-router-dom';
+import { Footer, Header } from './';
 
 const Error = () => {
   const err: any = useRouteError();
 
   const { status, statusText } = err;
-  
+
   return (
-    <>
-      <h2>Some error occured!</h2>
-      <h3>Status: {status}</h3>
-      <h4>Message: {statusText}</h4>
-    </>
+    <div data-testid='error' className='flex flex-col min-h-screen'>
+      <Header />
+      <div className='my-20 mx-0 px-4 py-2 sm:mx-4 md:px-8 md:mx-8 lg:px-12 lg:mx-16'>
+        <h2 className=''>Some error occured!</h2>
+        <h3 className=''>Status: {status}</h3>
+        <h4 className=''>Message: {statusText}</h4>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
