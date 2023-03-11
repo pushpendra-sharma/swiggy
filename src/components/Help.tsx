bd8faacb4a0b969d21ff7dd50827d7783648eca1
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-12 04:19:00 +0530

    feat: added questions accordion on help page

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,10 +1,16 @@
-import Header from './Header';
+import { useState } from 'react';
+import { HELP_DATA } from '../utils/constants';
+import { Accordion } from './';
 
 const Help = () => {
+  const [tab, setTab] = useState('Partner Onboarding');
+  const headings = Object.keys(HELP_DATA.data);
+  const questions = HELP_DATA.data[tab];
+
   return (
     <div className='mt-20'>
       <div className='mx-0 py-2 md:px-8 lg:px-12 md:bg-blue'>
-        <div className='px-4 sm:px-8 md:px-12 lg:px-16 md:pt-12 lg:pt-16 md:text-white'>
+        <div className='px-4 py-1 sm:px-8 md:px-12 lg:px-16 md:pt-12 lg:pt-16 md:text-white'>
           <h1 className='text-xl md:text-[32px] font-semibold capitalize'>
             Help & Support
           </h1>
@@ -13,101 +19,20 @@ const Help = () => {
           </h2>
         </div>
         <div className='py-4 md:p-8 sm:mx-8 md:mx-12 lg:mx-16 bg-white flex flex-col md:flex-row'>
-          <div className='hidden md:block bg-gray-thin pl-4 pt-4'>
-            <p className='text-gray-ultra p-4 md:pl-8 font-semibold bg-white'>
-              Partner Onboarding
-            </p>
-            <p className='text-gray-ultra p-4 md:pl-8 font-semibold'>Legal</p>
-            <p className='text-gray-ultra p-4 md:pl-8 font-semibold'>FAQs</p>
+          <div className='hidden bg-gray-thin pl-4 pt-4 md:flex flex-col'>
+            {headings.map(heading => (
+              <button
+                className={`text-gray-ultra p-4 md:pl-12 md:py-6 font-semibold ${
+                  tab === heading ? 'bg-white' : ''
+                }`}
+                key={heading}
+                onClick={() => setTab(heading)}
+              >
+                {heading}
+              </button>
+            ))}
           </div>
-          <div className='bg-gray-thin md:bg-white md:pl-8 w-full'>
-            <p className='text-gray-ultra py-4 font-medium uppercase md:capitalize  px-4 text-xs md:text-2xl md:font-semibold'>
-              Partner Onboarding
-            </p>
-            <div className='bg-white px-4 w-full'>
-              <div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div><div className='py-4 text-gray-ultra md:text-gray-medium border-b border-[#d4d5d9] flex gap-2 justify-between w-full'>
-                <span className='text-[13px] md:text-base'>
-                  What are the mandatory documents needed to list my restaurant
-                  on Swiggy?
-                </span>
-                <span className='material-symbols-outlined'>
-                  expand_more
-                </span>
-              </div>
-            </div>
-          </div>
+          <Accordion heading={tab} questions={questions} />
         </div>
       </div>
     </div>
@@ -115,17 +40,3 @@ const Help = () => {
 };
 
 export default Help;
-
-{
-  /* 
-
-<span class="material-symbols-outlined">
-expand_more
-</span>
-
-<span class="material-symbols-outlined">
-expand_less
-</span> 
-
-*/
-}
