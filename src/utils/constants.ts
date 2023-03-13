f00b874a1b29543d8d011ad08e3693665a16d7a7
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-13 07:15:58 +0530

    feat!: updated the api url
    Updated the API url endpoints to mock swiggy api
    to work without cors extension.
    
    Mock API deployed on https://swiggy-api.onrender.com.
    
    BREAKING CHANGE: APIs may fail without enabling cors extension

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,18 +4,18 @@ export const TITLE = 'Swiggy';
 
 export const CITIES = ['Noida'];
 
-export const BASE_URL = 'https://www.swiggy.com/dapi/';
+export const BASE_URL = 'https://swiggy-api.onrender.com';
 
 export const IMG_CDN_URL =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
 
 export const GET_ALL_RESTAURANT_URL =
-  '/restaurants/list/v5?lat=28.52572220267605&lng=77.39057801663876&page_type=DESKTOP_WEB_LISTING';
+  '/api/restaurants/list/v5?lat=28.52572220267605&lng=77.39057801663876&page_type=DESKTOP_WEB_LISTING';
 
 export const GET_RESTAURANT_DETAIL_URL =
-  '/menu/v4/full?lat=28.52572220267605&lng=77.39057801663876&menuId=';
+  '/api/restaurants/menu/v4/full?lat=28.52572220267605&lng=77.39057801663876&menuId=';
 
-export const SEARCH_URL = `/restaurants/search/suggest?lat=28.5770877&lng=77.3417748&str=`;
+export const SEARCH_URL = `/api/restaurants/search/suggest?lat=28.5770877&lng=77.3417748&str=`;
 
 export const HELP_DATA: HELP = {
   data: {
