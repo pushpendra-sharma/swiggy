6df66854ca430c7a07a3d1d27b68c9de4c28c0a4
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-13 07:20:35 +0530

    fix: updated url routes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,10 +10,10 @@ export const IMG_CDN_URL =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
 
 export const GET_ALL_RESTAURANT_URL =
-  '/api/restaurants/list/v5?lat=28.52572220267605&lng=77.39057801663876&page_type=DESKTOP_WEB_LISTING';
+  '/api/restaurants/list?lat=28.52572220267605&lng=77.39057801663876&page_type=DESKTOP_WEB_LISTING';
 
 export const GET_RESTAURANT_DETAIL_URL =
-  '/api/restaurants/menu/v4/full?lat=28.52572220267605&lng=77.39057801663876&menuId=';
+  '/api/restaurants/menu?lat=28.52572220267605&lng=77.39057801663876&menuId=';
 
 export const SEARCH_URL = `/api/restaurants/search/suggest?lat=28.5770877&lng=77.3417748&str=`;
 
